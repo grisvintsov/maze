ba1c9d0109d9f4db9d6ff5a2cbfa80992c1ab9a6
grisvintsov <40032224+grisvintsov@users.noreply.github.com>
2018-06-07 21:38:54 +0700

    main
    first

---
+++ player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace flashmaze
+{
+    class player
+    {
+        public int p_coordX { get; set; }
+        public int p_coordY { get; set; }
+    }
+}
